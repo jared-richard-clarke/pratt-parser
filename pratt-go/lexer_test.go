740aafc0ee943195b1644466d0ead5ea57d4ac9c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-08 18:59:45 -0800

    Expand test cases

--- pratt-go/lexer_test.go
+++ pratt-go/lexer_test.go
@@ -49,7 +49,55 @@ func TestScan(t *testing.T) {
 		e := expect[i]
 		r := *c
 		if e != r {
-			t.Errorf("Test failed. Expected: %v, Got: %v", e, r)
+			t.Errorf("Test Scan (Part 1) failed. Expected: %v, Got: %v", e, r)
+		}
+	}
+
+	text = "1 + 2\n * 3"
+	expect = []token{
+		{
+			typeof: number,
+			value:  1.0,
+			line:   1,
+			column: 1,
+		},
+		{
+			typeof: add,
+			value:  '+',
+			line:   1,
+			column: 3,
+		},
+		{
+			typeof: number,
+			value:  2.0,
+			line:   1,
+			column: 5,
+		},
+		{
+			typeof: mul,
+			value:  '*',
+			line:   2,
+			column: 2,
+		},
+		{
+			typeof: number,
+			value:  3.0,
+			line:   2,
+			column: 4,
+		},
+		{
+			typeof: eof,
+			value:  nil,
+			line:   2,
+			column: 5,
+		},
+	}
+	result, _ = Scan(text)
+	for i, c := range result {
+		e := expect[i]
+		r := *c
+		if e != r {
+			t.Errorf("Test Scan (Part 2) failed. Expected: %v, Got: %v", e, r)
 		}
 	}
 }
