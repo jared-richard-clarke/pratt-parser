5f9a9c2d69e5c68ff212b64eec6d0c663d828705
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-09 00:04:59 -0800

    Expand tests

--- pratt-go/lexer_test.go
+++ pratt-go/lexer_test.go
@@ -5,7 +5,9 @@ import (
 )
 
 func TestScan(t *testing.T) {
+
 	text := "1 + 2 * 3"
+
 	expect := []token{
 		{
 			typeof: number,
@@ -44,6 +46,7 @@ func TestScan(t *testing.T) {
 			column: 10,
 		},
 	}
+
 	result, _ := Scan(text)
 	for i, c := range result {
 		e := expect[i]
@@ -54,6 +57,7 @@ func TestScan(t *testing.T) {
 	}
 
 	text = "1 + 2\n * 3"
+
 	expect = []token{
 		{
 			typeof: number,
@@ -92,12 +96,127 @@ func TestScan(t *testing.T) {
 			column: 5,
 		},
 	}
+
 	result, _ = Scan(text)
 	for i, c := range result {
 		e := expect[i]
 		g := *c
 		if e != g {
 			t.Errorf("Test Scan (Part 2) failed. Expected: %v, Got: %v", e, g)
+		}
+	}
+
+	text = `1 +
+	        2 *
+	        3`
+
+	expect = []token{
+		{
+			typeof: number,
+			value:  1.0,
+			line:   1,
+			column: 1,
+		},
+		{
+			typeof: add,
+			value:  '+',
+			line:   1,
+			column: 3,
+		},
+		{
+			typeof: number,
+			value:  2.0,
+			line:   2,
+			column: 10,
+		},
+		{
+			typeof: mul,
+			value:  '*',
+			line:   2,
+			column: 12,
+		},
+		{
+			typeof: number,
+			value:  3.0,
+			line:   3,
+			column: 10,
+		},
+		{
+			typeof: eof,
+			value:  nil,
+			line:   3,
+			column: 11,
+		},
+	}
+
+	result, _ = Scan(text)
+	for i, c := range result {
+		e := expect[i]
+		g := *c
+		if e != g {
+			t.Errorf("Test Scan (Part 3) failed. Expected: %v, Got: %v", e, g)
+		}
+	}
+
+	text = "(1 + 2) * 3"
+
+	expect = []token{
+		{
+			typeof: oParen,
+			value:  '(',
+			line:   1,
+			column: 1,
+		},
+		{
+			typeof: number,
+			value:  1.0,
+			line:   1,
+			column: 2,
+		},
+		{
+			typeof: add,
+			value:  '+',
+			line:   1,
+			column: 4,
+		},
+		{
+			typeof: number,
+			value:  2.0,
+			line:   1,
+			column: 6,
+		},
+		{
+			typeof: cParen,
+			value:  ')',
+			line:   1,
+			column: 7,
+		},
+		{
+			typeof: mul,
+			value:  '*',
+			line:   1,
+			column: 9,
+		},
+		{
+			typeof: number,
+			value:  3.0,
+			line:   1,
+			column: 11,
+		},
+		{
+			typeof: eof,
+			value:  nil,
+			line:   1,
+			column: 12,
+		},
+	}
+
+	result, _ = Scan(text)
+	for i, c := range result {
+		e := expect[i]
+		g := *c
+		if e != g {
+			t.Errorf("Test Scan (Part 4) failed. Expected: %v, Got: %v", e, g)
 		}
 	}
 }
