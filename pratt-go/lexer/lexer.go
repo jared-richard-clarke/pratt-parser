5c02d3d899fe9920820588499963f9e76a76a232
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-15 22:08:00 -0800

    Make certain fields public

--- pratt-go/lexer/lexer.go
+++ pratt-go/lexer/lexer.go
@@ -18,47 +18,47 @@ const (
 type lexType int
 
 const (
-	lexOpenParen lexType = iota
-	lexCloseParen
-	lexAdd
-	lexSub
-	lexMul
-	lexDiv
-	lexExp
-	lexNumber
-	lexIdent
-	lexError
-	lexEOF
+	OpenParen lexType = iota
+	CloseParen
+	Add
+	Sub
+	Mul
+	Div
+	Exp
+	Number
+	Ident
+	Error
+	EOF
 )
 
-type token struct {
-	typeof lexType
-	value  string
-	line   int
-	column int
-	length int
+type Token struct {
+	Typeof lexType
+	Value  string
+	Line   int
+	Column int
+	Length int
 }
 
-func (t token) String() string {
+func (t Token) String() string {
 	switch {
-	case t.typeof < lexNumber:
-		return fmt.Sprintf("Punct: %q :%d:%d:%d", t.value, t.line, t.column, t.length)
-	case t.typeof == lexNumber:
-		return fmt.Sprintf("Float: %q :%d:%d:%d", t.value, t.line, t.column, t.length)
-	case t.typeof == lexIdent:
-		return fmt.Sprintf("Ident: %q :%d:%d:%d", t.value, t.line, t.column, t.length)
-	case t.typeof == lexError:
-		return fmt.Sprintf("Error: %q :%d:%d:%d", t.value, t.line, t.column, t.length)
-	case t.typeof == lexEOF:
-		return fmt.Sprintf("EOF :%d:%d:%d", t.line, t.column, t.length)
+	case t.Typeof < Number:
+		return fmt.Sprintf("Punct: %q :%d:%d:%d", t.Value, t.Line, t.Column, t.Length)
+	case t.Typeof == Number:
+		return fmt.Sprintf("Float: %q :%d:%d:%d", t.Value, t.Line, t.Column, t.Length)
+	case t.Typeof == Ident:
+		return fmt.Sprintf("Ident: %q :%d:%d:%d", t.Value, t.Line, t.Column, t.Length)
+	case t.Typeof == Error:
+		return fmt.Sprintf("Error: %q :%d:%d:%d", t.Value, t.Line, t.Column, t.Length)
+	case t.Typeof == EOF:
+		return fmt.Sprintf("EOF :%d:%d:%d", t.Line, t.Column, t.Length)
 	default:
-		return fmt.Sprintf("Undef: %q :%d:%d:%d", t.value, t.line, t.column, t.length)
+		return fmt.Sprintf("Undef: %q :%d:%d:%d", t.Value, t.Line, t.Column, t.Length)
 	}
 }
 
 type scanner struct {
 	source string
-	tokens []token
+	tokens []Token
 	length int // The number of bytes in the source string. Compute only once.
 
 	offset int // The total offset from the beginning of a string. Counts runes by byte size.
@@ -112,12 +112,12 @@ func (sc *scanner) addToken(t lexType, v string) {
 	// Ensure column count begins at start of lexeme.
 	column := sc.column - lexOffset
 
-	sc.tokens = append(sc.tokens, token{
-		typeof: t,
-		value:  v,
-		line:   sc.line,
-		column: column,
-		length: runeCount,
+	sc.tokens = append(sc.tokens, Token{
+		Typeof: t,
+		Value:  v,
+		Line:   sc.line,
+		Column: column,
+		Length: runeCount,
 	})
 }
 
@@ -133,25 +133,25 @@ func (sc *scanner) scanToken() {
 		return
 	// punctuators
 	case r == '(':
-		sc.addToken(lexOpenParen, "(")
+		sc.addToken(OpenParen, "(")
 		return
 	case r == ')':
-		sc.addToken(lexCloseParen, ")")
+		sc.addToken(CloseParen, ")")
 		return
 	case r == '-':
-		sc.addToken(lexSub, "-")
+		sc.addToken(Sub, "-")
 		return
 	case r == '+':
-		sc.addToken(lexAdd, "+")
+		sc.addToken(Add, "+")
 		return
 	case r == '*':
-		sc.addToken(lexMul, "*")
+		sc.addToken(Mul, "*")
 		return
 	case r == '/':
-		sc.addToken(lexDiv, "/")
+		sc.addToken(Div, "/")
 		return
 	case r == '^':
-		sc.addToken(lexExp, "^")
+		sc.addToken(Exp, "^")
 		return
 	// numbers
 	case unicode.IsDigit(r):
@@ -165,7 +165,7 @@ func (sc *scanner) scanToken() {
 			}
 		}
 		text := sc.source[sc.start:sc.offset]
-		sc.addToken(lexNumber, text)
+		sc.addToken(Number, text)
 		return
 	// identifiers
 	case unicode.IsLetter(r):
@@ -173,20 +173,20 @@ func (sc *scanner) scanToken() {
 			sc.next()
 		}
 		text := sc.source[sc.start:sc.offset]
-		sc.addToken(lexIdent, text)
+		sc.addToken(Ident, text)
 		return
 	// undefined
 	default:
-		sc.addToken(lexError, string(r))
+		sc.addToken(Error, string(r))
 		return
 	}
 }
 
 // The Lexer API: drives the scanner.
-func Scan(t string) []token {
+func Scan(t string) []Token {
 	sc := scanner{
 		source: t,
-		tokens: make([]token, 0),
+		tokens: make([]Token, 0),
 		length: len(t),
 		offset: 0,
 		start:  0,
@@ -197,11 +197,11 @@ func Scan(t string) []token {
 		sc.start = sc.offset
 		sc.scanToken()
 	}
-	sc.tokens = append(sc.tokens, token{
-		typeof: lexEOF,
-		line:   sc.line,
-		column: sc.column + 1,
-		length: 1,
+	sc.tokens = append(sc.tokens, Token{
+		Typeof: EOF,
+		Line:   sc.line,
+		Column: sc.column + 1,
+		Length: 1,
 	})
 	return sc.tokens
 }
