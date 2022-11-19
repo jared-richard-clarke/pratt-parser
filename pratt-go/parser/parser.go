7bb03392102478e2fd65f68f7e3d0b782fad8cae
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-18 19:42:04 -0800

    Revise comments

--- pratt-go/parser/parser.go
+++ pratt-go/parser/parser.go
@@ -3,8 +3,8 @@ package parser
 import "github/jared-richard-clarke/pratt/lexer"
 
 type parser struct {
-	src   []lexer.Token
-	token lexer.Token // Current token.
-	index int         // Current index in src.
-	ast   Expr        // Abstract Syntax Tree.
+	src   []lexer.Token // Token source.
+	token lexer.Token   // Current token.
+	index int           // Current index in src.
+	ast   Expr          // Abstract Syntax Tree.
 }
