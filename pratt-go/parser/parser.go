b5ac7956642093c27d4c37dc6221d5b0038bda42
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-20 15:54:47 -0800

    Rename fields and types

--- pratt-go/parser/parser.go
+++ pratt-go/parser/parser.go
@@ -5,9 +5,9 @@ import "github/jared-richard-clarke/pratt/lexer"
 type nud func(lexer.Token) Node       // Null denotation
 type led func(Node, lexer.Token) Node // Left denotation
 
-type parseTables struct {
-	nuds map[lexer.LexType]nud // lexeme -> Nud
-	leds map[lexer.LexType]led // lexeme -> Led
+type parseTable struct {
+	nudParse map[lexer.LexType]nud // lexeme -> Nud
+	ledParse map[lexer.LexType]led // lexeme -> Led
 
 	nudBind map[lexer.LexType]int // lexeme -> prefix precedence
 	ledBind map[lexer.LexType]int // lexeme -> infix precedence
