74ed9d08ecf742f33a96d3f69a6dc7f5c95f9d02
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-19 18:42:38 -0800

    Reformat

--- pratt-go/parser/parser.go
+++ pratt-go/parser/parser.go
@@ -3,10 +3,10 @@ package parser
 import "github/jared-richard-clarke/pratt/lexer"
 
 type (
-	Nud     func(lexer.Token) Node              // Null denotation
-	Led     func(left Node, t lexer.Token) Node // Left denotation
-	NudMap  map[lexer.LexType]Nud               // token -> Nud
-	LedMap  map[lexer.LexType]Led               // token -> Led
-	NudBind map[lexer.LexType]int               // prefix binding powers
-	LedBind map[lexer.LexType]int               // infix binding powers
+	Nud     func(lexer.Token) Node       // Null denotation
+	Led     func(Node, lexer.Token) Node // Left denotation
+	NudMap  map[lexer.LexType]Nud        // lexeme -> Nud
+	LedMap  map[lexer.LexType]Led        // lexeme -> Led
+	NudBind map[lexer.LexType]int        // prefix binding powers
+	LedBind map[lexer.LexType]int        // infix binding powers
 )
