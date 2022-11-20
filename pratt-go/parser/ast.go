6f180e23f9320e4f2fc8a0248ccc09173c01b68e
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-19 17:14:38 -0800

    Create ast.go

---
+++ pratt-go/parser/ast.go
@@ -0,0 +1,55 @@
+package parser
+
+import (
+	"fmt"
+	"pratt/lexer"
+)
+
+type position struct {
+	line   int
+	column int
+	length int
+}
+
+type Node interface {
+	Pos() string
+}
+
+type Number struct {
+	Value float64
+	position
+}
+
+func (n *Number) Pos() string {
+	return fmt.Sprintf("lin %d, col %d, len %d", n.line, n.column, n.length)
+}
+
+type Unary struct {
+	Op lexer.LexType
+	X  Node
+	position
+}
+
+func (u *Unary) Pos() string {
+	return fmt.Sprintf("lin %d, col %d, len %d", u.line, u.column, u.length)
+}
+
+type Binary struct {
+	Op lexer.LexType
+	X  Node
+	Y  Node
+	position
+}
+
+func (b *Binary) Pos() string {
+	return fmt.Sprintf("lin %d, col %d, len %d", b.line, b.column, b.length)
+}
+
+type Ident struct {
+	Id string
+	position
+}
+
+func (i *Ident) Pos() string {
+	return fmt.Sprintf("lin %d, col %d, len %d", i.line, i.column, i.length)
+}
