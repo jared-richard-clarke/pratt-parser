d5a851481a3ea226870c74a149fd59ab7d51b05e
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-19 17:32:50 -0800

    Expand type definitions

--- pratt-go/parser/parser.go
+++ pratt-go/parser/parser.go
@@ -2,6 +2,9 @@ package parser
 
 import "github/jared-richard-clarke/pratt/lexer"
 
-type Nud func(lexer.Token) Node
-
-type Led func(left Node, t lexer.Token) Node
+type (
+	Nud  func(lexer.Token) Node              // Null denotation
+	Led  func(left Node, t lexer.Token) Node // Left denotation
+	Nuds map[lexer.LexType]Nud               // lexeme -> Nud
+	Leds map[lexer.LexType]Led               // lexeme -> Led
+)
