b82da382cbe33b80d540016da703d0eeeec8c4fc
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-20 18:09:06 -0800

    Greatly expand parser

--- pratt-go/parser/parser.go
+++ pratt-go/parser/parser.go
@@ -2,18 +2,107 @@ package parser
 
 import "github/jared-richard-clarke/pratt/lexer"
 
+// === Under Heavy Construction ===
+
 type nud func(lexer.Token) Node       // Null denotation
 type led func(Node, lexer.Token) Node // Left denotation
 
-type parseTable struct {
-	nudParse map[lexer.LexType]nud // lexeme -> Nud
-	ledParse map[lexer.LexType]led // lexeme -> Led
+type table struct {
+	nuds map[lexer.LexType]nud // lexeme -> Nud
+	leds map[lexer.LexType]led // lexeme -> Led
 
-	nudBind map[lexer.LexType]int // lexeme -> prefix precedence
-	ledBind map[lexer.LexType]int // lexeme -> infix precedence
+	prefix map[lexer.LexType]int // lexeme -> prefix precedence
+	infix  map[lexer.LexType]int // lexeme -> infix precedence
 }
 
 type parser struct {
-	src   []lexer.Token
-	index int
+	src    []lexer.Token
+	length int
+	index  int
+	table
+}
+
+func (p *parser) next() lexer.Token {
+	t := p.src[p.index]
+	p.index += 1
+	return t
+}
+
+func (p *parser) expression(rbp int) Node {
+	token := p.next()
+	nud, ok := p.table.nuds[token.Typeof]
+	if !ok {
+		panic(fmt.Errorf("Could not parse %s", token.Value))
+	}
+	left := nud(token)
+	for rbp < p.table.infix[token.Typeof] {
+		token := p.next()
+		led := p.table.leds[token.Typeof]
+		left = led(left, token)
+	}
+	return left
+}
+
+func (p *parser) parseNumber(t lexer.Token) Node {
+	num, _ := strconv.ParseFloat(t.Value, 64)
+	return &Number{
+		Value: num,
+		position: position{
+			line:   t.Line,
+			column: t.Column,
+			length: t.Length,
+		},
+	}
+}
+
+func (p *parser) parseIdent(t lexer.Token) Node {
+	return &Ident{
+		Id: t.Value,
+		position: position{
+			line:   t.Line,
+			column: t.Column,
+			length: t.Length,
+		},
+	}
+}
+
+func (p *parser) parseUnary(t lexer.Token) Node {
+	x := p.expression(p.prefix[t.Typeof])
+	return &Unary{
+		Op: t.Typeof,
+		X:  x,
+		position: position{
+			line:   t.Line,
+			column: t.Column,
+			length: t.Length,
+		},
+	}
+}
+
+func (p *parser) parseBinary(left Node, t lexer.Token) Node {
+	right := p.expression(p.table.infix[t.Typeof])
+	return &Binary{
+		Op: t.Typeof,
+		X:  left,
+		Y:  right,
+		position: position{
+			line:   t.Line,
+			column: t.Column,
+			length: t.Length,
+		},
+	}
+}
+
+func (p *parser) parseBinaryRight(left Node, t lexer.Token) Node {
+	right := p.expression(p.table.infix[t.Typeof] - 1)
+	return &Binary{
+		Op: t.Typeof,
+		X:  left,
+		Y:  right,
+		position: position{
+			line:   t.Line,
+			column: t.Column,
+			length: t.Length,
+		},
+	}
 }
