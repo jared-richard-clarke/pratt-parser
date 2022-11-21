f84883ce1d4763bac700d3401bd4bdbfa90f43a3
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-20 19:12:40 -0800

    Simplify Node interface

--- pratt-go/parser/ast.go
+++ pratt-go/parser/ast.go
@@ -1,55 +1,31 @@
 package parser
 
-import (
-	"fmt"
-	"github/jared-richard-clarke/pratt/lexer"
-)
-
-type position struct {
-	line   int
-	column int
-	length int
-}
+import "github/jared-richard-clarke/pratt/lexer"
 
 type Node interface {
-	Pos() string
+	ast()
 }
 
 type Number struct {
 	Value float64
-	position
-}
-
-func (n *Number) Pos() string {
-	return fmt.Sprintf("lin %d, col %d, len %d", n.line, n.column, n.length)
 }
 
 type Unary struct {
 	Op lexer.LexType
 	X  Node
-	position
-}
-
-func (u *Unary) Pos() string {
-	return fmt.Sprintf("lin %d, col %d, len %d", u.line, u.column, u.length)
 }
 
 type Binary struct {
 	Op lexer.LexType
 	X  Node
 	Y  Node
-	position
-}
-
-func (b *Binary) Pos() string {
-	return fmt.Sprintf("lin %d, col %d, len %d", b.line, b.column, b.length)
 }
 
 type Ident struct {
-	Id string
-	position
+	Value string
 }
 
-func (i *Ident) Pos() string {
-	return fmt.Sprintf("lin %d, col %d, len %d", i.line, i.column, i.length)
-}
+func (n *Number) ast() {}
+func (u *Unary) ast()  {}
+func (b *Binary) ast() {}
+func (i *Ident) ast()  {}
