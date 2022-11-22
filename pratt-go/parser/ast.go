27cf4e17a276cecf0c30927fab00637b3a2e6585
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-22 15:59:29 -0800

    Revise fields

--- pratt-go/parser/ast.go
+++ pratt-go/parser/ast.go
@@ -1,7 +1,5 @@
 package parser
 
-import "github/jared-richard-clarke/pratt/lexer"
-
 type Node interface {
 	ast()
 }
@@ -17,18 +15,18 @@ type Ident struct {
 }
 
 type Unary struct {
-	Op           lexer.LexType
+	Op           string
 	X            Node
 	Line, Column int
 }
 
 type Binary struct {
-	Op           lexer.LexType
+	Op           string
 	X, Y         Node
 	Line, Column int
 }
 
 func (n *Number) ast() {}
+func (i *Ident) ast()  {}
 func (u *Unary) ast()  {}
 func (b *Binary) ast() {}
-func (i *Ident) ast()  {}
