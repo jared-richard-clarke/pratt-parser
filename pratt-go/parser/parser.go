8b882fb042205764161846efbd2d39614f520838
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-22 23:01:49 -0800

    Setup parser initialization

--- pratt-go/parser/parser.go
+++ pratt-go/parser/parser.go
@@ -23,7 +23,7 @@ type parser struct {
 	length int           // len(src)
 	index  int           // src[index]
 	end    int           // src[len(src) - 1]
-	table                // parser and binding lookup
+	*table               // parser and binding lookup
 }
 
 func (p *parser) next() lexer.Token {
@@ -34,10 +34,6 @@ func (p *parser) next() lexer.Token {
 	t := p.src[p.index]
 	p.index += 1
 	return t
-}
-
-func (p *parser) peek() lexer.LexType {
-	return p.src[p.index].Typeof
 }
 
 func (p *parser) match(e lexer.LexType) bool {
@@ -114,7 +110,7 @@ func (p *parser) parseBinary(left Node, t lexer.Token) (Node, error) {
 	}, nil
 }
 
-func (p *parser) parseBinaryR(left Node, t lexer.Token) (Node, error) {
+func (p *parser) parseBinaryRight(left Node, t lexer.Token) (Node, error) {
 	right, err := p.parseExpr(p.lbp[t.Typeof] - 1)
 	if err != nil {
 		return nil, err
@@ -128,7 +124,7 @@ func (p *parser) parseBinaryR(left Node, t lexer.Token) (Node, error) {
 	}, nil
 }
 
-func (p *parser) parseParenExpr(t lexer.Token) (Node, error) {
+func (p *parser) parseParenPrefix(t lexer.Token) (Node, error) {
 	x, err := p.parseExpr(0)
 	if err != nil {
 		return nil, err
@@ -137,4 +133,64 @@ func (p *parser) parseParenExpr(t lexer.Token) (Node, error) {
 		return nil, fmt.Errorf("expected ')', got '%s'", t.Value)
 	}
 	return x, nil
+}
+
+// Carries parser's internal state. Should persist throughout package lifetime.
+var pratt parser
+
+func init() {
+	// Build lookup table at package initialization.
+	pratt = parser{
+		table: &table{
+			nuds: make(map[lexer.LexType]nud),
+			leds: make(map[lexer.LexType]led),
+			rbp:  make(map[lexer.LexType]int),
+			lbp:  make(map[lexer.LexType]int),
+		},
+	}
+	// Helper functions build lookup tables.
+	addNud := func(bp int, t lexer.LexType, n nud) {
+		pratt.nuds[t] = n
+		pratt.rbp[t] = bp
+	}
+	addLed := func(t lexer.LexType, bp int, l led) {
+		pratt.leds[t] = l
+		pratt.lbp[t] = bp
+	}
+	unary := func(bp int, ts ...lexer.LexType) {
+		for _, t := range ts {
+			addNud(bp, t, pratt.parseUnary)
+		}
+	}
+	binary := func(bp int, ts ...lexer.LexType) {
+		for _, t := range ts {
+			addLed(t, bp, pratt.parseBinary)
+		}
+	}
+	binaryRight := func(bp int, ts ...lexer.LexType) {
+		for _, t := range ts {
+			addLed(t, bp, pratt.parseBinaryRight)
+		}
+	}
+	// Initialize lookup tables.
+	addNud(0, lexer.OpenParen, pratt.parseParenPrefix)
+	addNud(0, lexer.Ident, pratt.parseIdent)
+	addNud(0, lexer.Number, pratt.parseNumber)
+	binary(50, lexer.Add, lexer.Sub)
+	binary(60, lexer.Mul, lexer.Div)
+	binaryRight(70, lexer.Pow)
+	unary(80, lexer.Add, lexer.Sub)
+}
+
+// Parser API: inputs tokens, outputs either AST or Error
+func Parse(ts []lexer.Token) (Node, error) {
+	// Set parser state.
+	pratt = parser{
+		src:    ts,
+		length: len(ts),
+		index:  0,
+		end:    len(ts) - 1,
+		table:  pratt.table, // Persist lookup table from package initialization.
+	}
+	return nil, nil
 }
