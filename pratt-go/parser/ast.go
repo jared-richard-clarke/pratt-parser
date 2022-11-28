2d437a5a9718a698b4ee98a63e269d6dd1ed1ea1
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-11-28 11:11:04 -0800

    Implement Stringer for Node

--- pratt-go/parser/ast.go
+++ pratt-go/parser/ast.go
@@ -12,16 +12,31 @@ type Literal struct {
 	Line, Column int
 }
 
+func (l *Literal) String() string {
+	s := "Literal{ Value: %s }"
+	return fmt.Sprintf(s, l.Value)
+}
+
 type Unary struct {
 	Op           string
 	X            Node
 	Line, Column int
 }
 
+func (u *Unary) String() string {
+	s := "Unary{ Op: %s, X: %s }"
+	return fmt.Sprintf(s, u.Op, u.X)
+}
+
 type Binary struct {
 	Op           string
 	X, Y         Node
 	Line, Column int
+}
+
+func (b *Binary) String() string {
+	s := "Binary{ Op: %s, X: %s, Y: %s }"
+	return fmt.Sprintf(s, b.Op, b.X, b.Y)
 }
 
 func (l *Literal) ast() {}
