9d4c702bae69aad6826490e400e0f9f385e6550f
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-28 14:40:00 -0800

    Remove ImpliedBinary

--- pratt-go/parser/ast.go
+++ pratt-go/parser/ast.go
@@ -56,32 +56,20 @@ func (b *Binary) String() string {
 	return fmt.Sprintf(msg, b.Op, b.X, b.Y)
 }
 
-// Like Binary but without positional information.
-type ImpBinary struct {
-	Op   string
-	X, Y Node
-}
-
-func (i *ImpBinary) String() string {
-	msg := "ImpBinary{ Op: %s, X: %s, Y: %s }"
-	return fmt.Sprintf(msg, i.Op, i.X, i.Y)
-}
-
 type Call struct {
-	Fun          Node
+	Callee       Node
 	Args         []Node
 	Line, Column int
 }
 
 func (c *Call) String() string {
-	msg := "Call{ Fun: %v, Args: %v }"
-	return fmt.Sprintf(msg, c.Fun, c.Args)
+	msg := "Call{ Callee: %v, Args: %v }"
+	return fmt.Sprintf(msg, c.Callee, c.Args)
 }
 
-func (e *Empty) ast()     {}
-func (n *Number) ast()    {}
-func (s *Symbol) ast()     {}
-func (u *Unary) ast()     {}
-func (b *Binary) ast()    {}
-func (i *ImpBinary) ast() {}
-func (f *Call) ast()      {}
+func (e *Empty) ast()  {}
+func (n *Number) ast() {}
+func (s *Symbol) ast() {}
+func (u *Unary) ast()  {}
+func (b *Binary) ast() {}
+func (f *Call) ast()   {}
