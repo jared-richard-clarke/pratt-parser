1e6299cc987ee01876760e9b7ed2a7b26c4eef08
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-28 15:39:55 -0800

    Reinsert ImpliedBinary

--- pratt-go/parser/ast.go
+++ pratt-go/parser/ast.go
@@ -56,6 +56,17 @@ func (b *Binary) String() string {
 	return fmt.Sprintf(msg, b.Op, b.X, b.Y)
 }
 
+// Like Binary but without positional information.
+type ImpliedBinary struct {
+	Op   string
+	X, Y Node
+}
+
+func (i *ImpliedBinary) String() string {
+	msg := "ImpliedBinary{ Op: %s, X: %s, Y: %s }"
+	return fmt.Sprintf(msg, i.Op, i.X, i.Y)
+}
+
 type Call struct {
 	Callee       Node
 	Args         []Node
@@ -67,9 +78,10 @@ func (c *Call) String() string {
 	return fmt.Sprintf(msg, c.Callee, c.Args)
 }
 
-func (e *Empty) ast()  {}
-func (n *Number) ast() {}
-func (s *Symbol) ast() {}
-func (u *Unary) ast()  {}
-func (b *Binary) ast() {}
-func (f *Call) ast()   {}
+func (e *Empty) ast()         {}
+func (n *Number) ast()        {}
+func (s *Symbol) ast()        {}
+func (u *Unary) ast()         {}
+func (b *Binary) ast()        {}
+func (i *ImpliedBinary) ast() {}
+func (f *Call) ast()          {}
