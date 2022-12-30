3a5b1727477369d03ebd56768b36db65952f03e0
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-29 18:30:19 -0800

    Refine error messaging for unused tokens

--- pratt-go/parser/parser.go
+++ pratt-go/parser/parser.go
@@ -261,16 +261,22 @@ func Parse(s string) (Node, error) {
 	if err != nil {
 		return nil, err
 	}
-	// If extra tokens at end of expression, return error.
+	// If unused tokens at end of expression, return error.
 	if pratt.index < pratt.end {
+		var a strings.Builder
+		a.WriteString("\"")
+		for i := 0; i < pratt.index; i += 1 {
+			a.WriteString(pratt.src[i].Value)
+		}
+		a.WriteString("\"")
 		var b strings.Builder
 		b.WriteString("[ ")
 		for i := pratt.index; i < pratt.end; i += 1 {
 			b.WriteString(pratt.src[i].Value + " ")
 		}
 		b.WriteString("]")
-		msg := "extra token(s) %s at end of expression"
-		return nil, fmt.Errorf(msg, b.String())
+		msg := "unused token(s) %s at end of expression %s"
+		return nil, fmt.Errorf(msg, b.String(), a.String())
 	}
 	return node, nil
 }
