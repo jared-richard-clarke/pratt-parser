63bffa006456971cbee2497b6b003339c5989e33
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2022-12-30 12:53:37 -0800

    Refine error handling

--- pratt-go/parser/parser.go
+++ pratt-go/parser/parser.go
@@ -267,13 +267,6 @@ func Parse(s string) (Node, error) {
 	}
 	// If unused tokens at end of expression, return error.
 	if pratt.index < pratt.end {
-		// Build string from expression.
-		var a strings.Builder
-		a.WriteString("\"" + " ")
-		for i := 0; i < pratt.index; i += 1 {
-			a.WriteString(pratt.src[i].Value + " ")
-		}
-		a.WriteString("\"")
 		// Build string from list of unused tokens.
 		var b strings.Builder
 		b.WriteString("[" + " ")
@@ -282,8 +275,8 @@ func Parse(s string) (Node, error) {
 		}
 		b.WriteString("]")
 		// Compose error message.
-		msg := "unused token(s) %s at end of expression %s"
-		return nil, fmt.Errorf(msg, b.String(), a.String())
+		msg := "unused token(s) %s at end of expression"
+		return nil, fmt.Errorf(msg, b.String())
 	}
 	return node, nil
 }
