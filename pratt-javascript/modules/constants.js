9545b1a3d4b67f349bd461cb74809cb67716c388
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-07-31 15:53:39 -0700

    Create constants.js

---
+++ pratt-javascript/modules/constants.js
@@ -0,0 +1,29 @@
+export const WHITE_SPACE = " ";
+export const TAB = "\t";
+export const LINEFEED = "\n";
+export const CARRIAGE_RETURN = "\r";
+export const VERTICAL_TAB = "\v";
+export const FORM_FEED = "\f";
+
+export const DECIMAL_POINT = ".";
+export const OPEN_PAREN = "(";
+export const CLOSE_PAREN = ")";
+
+export const EOF = "eof";
+
+export const ZERO = "0";
+export const ONE = "1";
+export const TWO = "2";
+export const THREE = "3";
+export const FOUR = "4";
+export const FIVE = "5";
+export const SIX = "6";
+export const SEVEN = "7";
+export const EIGHT = "8";
+export const NINE = "9";
+
+export const ADD = "+";
+export const SUBTRACT = "-";
+export const MULTIPLY = "×";
+export const DIVIDE = "÷";
+export const EXPONENT = "^";
