dbe892be23872965c905690d502868dcd8cb5afa
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-08-14 18:52:59 -0700

    Create utils.js

---
+++ pratt-javascript/modules/big-math/utils.js
@@ -0,0 +1,31 @@
+import constants from "./constants.js";
+
+function is_zero(x) {
+    return x.coefficient === constants.BIGINT_ZERO;
+}
+function is_negative(x) {
+    return x.coefficient < constants.BIGINT_ZERO;
+}
+function is_positive(x) {
+    return x.coefficient >= constants.BIGINT_ZERO;
+}
+function bigint_abs(x) {
+    return x < constants.BIGINT_ZERO ? -x : x;
+}
+function bigint_signum(x) {
+    if (x === constants.BIGINT_ZERO) {
+        return constants.BIGINT_ZERO;
+    }
+    if (x < constants.BIGINT_ZERO) {
+        return constants.BIGINT_NEG_ONE;
+    }
+    return constants.BIGINT_ONE;
+}
+
+export default Object.freeze({
+    is_zero,
+    is_negative,
+    is_positive,
+    bigint_abs,
+    bigint_signum,
+});
