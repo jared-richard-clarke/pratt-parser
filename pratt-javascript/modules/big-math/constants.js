24de41d02af9609daeeb9974911cd0aafa455672
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-08-14 18:53:44 -0700

    Add constants

--- pratt-javascript/modules/big-math/constants.js
+++ pratt-javascript/modules/big-math/constants.js
@@ -1,5 +1,7 @@
+const BIGINT_NEG_ONE = -1n;
 const BIGINT_ZERO = 0n;
 const BIGINT_ONE = 1n;
+const BIGINT_TWO = 2n;
 const BIGINT_TEN = 10n;
 const BIGINT_TEN_MILLION = 10000000n;
 
@@ -10,13 +12,30 @@ const ZERO = (function () {
     return Object.freeze(x);
 })();
 
+const ONE = (function() {
+    const x = Object.create(null);
+    x.coefficient = BIGINT_ONE;
+    x.exponent = 0;
+    return Object.freeze(x);
+})();
+
+const TWO = (function(){
+    const x = Object.create(null);
+    x.coefficient = BIGINT_TWO;
+    x.exponent = 0;
+    return Object.freeze(x);
+})();
+
 const PRECISION = -4;
 
 export default Object.freeze({
+    BIGINT_NEG_ONE,
     BIGINT_ZERO,
     BIGINT_ONE,
     BIGINT_TEN,
     BIGINT_TEN_MILLION,
     ZERO,
+    ONE,
+    TWO,
     PRECISION,
 });
