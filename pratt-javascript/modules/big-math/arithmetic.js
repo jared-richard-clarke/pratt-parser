8b10e974dc4a9873e75f98e4848065a34f54756b
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-08-15 18:43:07 -0700

    Add exponentiation

--- pratt-javascript/modules/big-math/arithmetic.js
+++ pratt-javascript/modules/big-math/arithmetic.js
@@ -5,10 +5,23 @@ import utils from "./utils.js";
 function neg(x) {
     return encoders.make_bigfloat(-x.coefficient, x.exponent);
 }
-function abs(x) {
-    return x.coefficient < constants.BIGINT_ZERO ? neg(x) : x;
+function floor(x) {
+    const { coefficient, exponent } = x;
+    if (exponent === 0) {
+        return x;
+    }
+    if (exponent > 0) {
+        return encoders.make_bigfloat(
+            coefficient * constants.BIGINT_TEN ** BigInt(exponent),
+            0,
+        );
+    }
+    return encoders.make_bigfloat(
+        coefficient / constants.BIGINT_TEN ** BigInt(-exponent),
+        0,
+    );
 }
-function conform_operation(op) {
+function adjust_terms(op) {
     return function (x, y) {
         const differential = x.exponent - y.exponent;
         if (differential === 0) {
@@ -37,15 +50,9 @@ function conform_operation(op) {
     };
 }
 
-const add = conform_operation((x, y) => x + y);
-const sub = conform_operation((x, y) => x - y);
+const add = adjust_terms((x, y) => x + y);
+const sub = adjust_terms((x, y) => x - y);
 
-function eq(x, y) {
-    return utils.is_zero(sub(x, y));
-}
-function lt(x, y) {
-    return utils.is_negative(sub(x, y));
-}
 function gt(x, y) {
     return utils.is_positive(sub(x, y));
 }
@@ -59,7 +66,7 @@ function mul(x, y) {
 
 function div(x, y) {
     if (utils.is_zero(x)) {
-        return constants.ZERO;
+        return constants.BIGFLOAT_ZERO;
     }
     if (utils.is_zero(y)) {
         return undefined;
@@ -82,4 +89,27 @@ function div(x, y) {
         coefficient += utils.bigint_signum(x.coefficient);
     }
     return encoders.make_bigfloat(coefficient, exponent);
+}
+
+// Exponentiation by squaring.
+function pow(x, y) {
+    if (utils.is_zero(x)) {
+        return constants.BIGFLOAT_ZERO;
+    }
+    if (utils.is_zero(y)) {
+        return constants.BIGFLOAT_ONE;
+    }
+    if (utils.is_negative(y)) {
+        x = div(constants.BIGFLOAT_ONE, x);
+        y = neg(y);
+    }
+    let z = constants.BIGFLOAT_ONE;
+    while (gt(y, constants.BIGFLOAT_ONE)) {
+        if (utils.is_odd(y)) {
+            z = mul(x, z);
+        }
+        x = mul(x, x);
+        y = floor(div(y, constants.BIGFLOAT_TWO));
+    }
+    return mul(x, z);
 }
